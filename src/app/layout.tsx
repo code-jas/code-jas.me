ff8517446622d0f8ae372a9b011136591516aa72
code-jas <johnangelo.silvestre04@gmail.com>
2024-07-10 01:12:48 +0800

    step

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 
 import { gitlabmono, incognito, ttNorms } from '@/app/assets/font/font';
 import ClientLayout from './ClientLayout';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="en" suppressHydrationWarning>
+         <Head>
+            <link rel="icon" href="/src/app/favicon.ico" type="image/x-icon" />
+         </Head>
          <body
             className={`${incognito.variable} ${inter.className} ${gitlabmono.variable} ${ttNorms.variable} font-tt-norms dark:bg-zinc-900 bg-white dark:text-white text-dark`}
          >
